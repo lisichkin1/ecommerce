ab89e879bd610426a95a299c2d0bcc59fc4f83a5
ivan <ivanlisichkin@icloud.com>
2023-10-12 13:39:37 +0400

    feat: add session

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,4 +1,11 @@
 import Layout from '@/components/Layout';
+import { useSession } from 'next-auth/react';
 export default function Component() {
-  return <Layout>test</Layout>;
+  const { data: session } = useSession();
+  if (!session) return;
+  return (
+    <Layout>
+      <div>Привет, {session?.user?.name}</div>
+    </Layout>
+  );
 }
