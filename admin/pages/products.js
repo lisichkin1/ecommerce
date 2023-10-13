50ed718f4e0cfd151f57c965096928c39c4940fa
ivan <ivanlisichkin@icloud.com>
2023-10-13 12:59:16 +0400

    feat: add button new product

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -1,5 +1,12 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 
 export default function products() {
-  return <Layout>страница продуктов</Layout>;
+  return (
+    <Layout>
+      <Link className="bg-green-400 p-2 rounded-lg pb-3 text-white" href={'products/new'}>
+        Добавить новый товар
+      </Link>
+    </Layout>
+  );
 }
