001bc806e3068a1f6e971ba100cd37c18b772dfd
ivan <ivanlisichkin@icloud.com>
2023-10-13 17:44:23 +0400

    feat: add form and post request

--- admin/pages/products/new.js
+++ admin/pages/products/new.js
@@ -1,17 +1,47 @@
 import Layout from '@/components/Layout';
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export default function NewProduct() {
+  const [title, setTitle] = useState('');
+  const [descriprion, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  console.log(title, descriprion, price);
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, descriprion, price };
+    await axios.post('/api/products', data);
+  }
   return (
     <Layout>
-      <h1>Новый товар</h1>
-      <label>Название товара</label>
-      <input className="input-product" type="text" placeholder="Введите название товара" />
-      <label>Описание</label>
-      <textarea placeholder="Введите описание" />
-      <label>Цена(руб)</label>
-      <input className="input-product" type="text" placeholder="Введите цену" />
-      <button className="btn-primary">Сохранить</button>
+      <form onSubmit={createProduct}>
+        <h1>Новый товар</h1>
+        <label>Название товара</label>
+        <input
+          className="input-product"
+          type="text"
+          placeholder="Введите название товара"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        />
+        <label>Описание</label>
+        <textarea
+          placeholder="Введите описание"
+          value={descriprion}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
+        <label>Цена(руб)</label>
+        <input
+          className="input-product"
+          type="text"
+          placeholder="Введите цену"
+          value={price}
+          onChange={(ev) => setPrice(ev.target.value)}
+        />
+        <button type="submit" className="btn-primary">
+          Сохранить
+        </button>
+      </form>
     </Layout>
   );
 }
