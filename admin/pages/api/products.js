8a6ab0d22dbd2f349eae596355558f57d9cb5609
ivan <ivanlisichkin@icloud.com>
2023-10-14 23:54:22 +0400

    feat: add a get request to get data

--- admin/pages/api/products.js
+++ admin/pages/api/products.js
@@ -1,12 +1,42 @@
-import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import {
+  collection,
+  addDoc,
+  getDoc,
+  querySnapshot,
+  query,
+  getDocs,
+  onSnapshot,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 
 export default async function handle(req, res) {
   const { method } = req;
+  if (req.method === 'GET') {
+    try {
+      const productsCollection = collection(db, 'products');
+      const productsQuery = query(productsCollection);
 
+      const snapshot = await getDocs(productsQuery);
+
+      const products = [];
+      snapshot.forEach((doc) => {
+        products.push({ id: doc.id, ...doc.data() });
+      });
+
+      res.status(200).json(products);
+    } catch (error) {
+      console.error('Ошибка при получении товаров:', error);
+      res.status(500).json({ message: 'Произошла ошибка при получении товаров' });
+    }
+  } else {
+    res.status(405).end(); // Метод не поддерживается
+  }
   if (method === 'POST') {
     try {
       const { title, description, price } = req.body;
+
       const newDocRef = await addDoc(collection(db, 'products'), {
         title: title.trim(),
         description: description,
