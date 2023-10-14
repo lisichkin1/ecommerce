68eaa7c01c49adb1e7192bc7740767014dc408fa
ivan <ivanlisichkin@icloud.com>
2023-10-14 23:53:45 +0400

    feat:add getting items from the database

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -1,7 +1,15 @@
 import Layout from '@/components/Layout';
+import axios from 'axios';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 export default function products() {
+  useEffect(() => {
+    axios.get('/api/products').then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
   return (
     <Layout>
       <Link className="bg-green-400 p-2 rounded-lg pb-3 text-white" href={'products/new'}>
